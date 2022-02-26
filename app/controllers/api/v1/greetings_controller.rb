2d1e24c3d90a3085de326b997c47bf0f015a833a
Cathella <ncathie214@gmail.com>
2022-02-27 00:42:51 +0300

    update greetings controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
-    
+    greeting = Greeting.order(Arel.sql('RANDOM()')).first
+    render json: { message: greeting.message }
   end
 end
