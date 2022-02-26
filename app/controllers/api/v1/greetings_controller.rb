cd0c3b89edbdafa2f43867843be727473ed52203
Cathella <ncathie214@gmail.com>
2022-02-27 00:32:23 +0300

    Add package.json, update README

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,10 +1,5 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
-    render json: { :greetings => [
-      {
-        :greeting => 'Hello',
-        :language => 'English'
-      }
-    ] }.to_json
+    
   end
 end
